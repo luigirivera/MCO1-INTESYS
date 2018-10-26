416930a6b25ce32583f00b256701a7e6e9a5ec59
luigirivera <luigirivera@users.noreply.github.com>
2018-10-26 21:45:19 +0800

    Cleaned code

--- GSG Student Copy/AI/PLAYERAI.js
+++ GSG Student Copy/AI/PLAYERAI.js
@@ -1,35 +1,36 @@
 var openList = [];
 var closedList = [];
-var start;
-var hasMoved;
-var end;
 var nextMove;
 var next;
 var grid;
 var cols;
 var rows;
-var direct = [[0,-1],[0,1],[-1,0],[1,0]];
 var path = [];
-    var moveCur;
-    var movePrev;
+var moveCur;
 var pathFound;
 var index;
 // Base functions
 
 // This function is run during the game loop repeatedly.
+// A*
 function THINK(player,enemies,maplayout,end)
 {
+    //if there are still tiles and there is no path yet
     if(openList.length > 0 && !pathFound)
     {
         var lowestIndex = 0;
+        //find the lowest cost of the list
         for(var i = 0; i<openList.length; i++)
             if(openList[i].f < openList[lowestIndex].f)
                 lowestIndex = i;
         
+        //hold that tile
         var current = openList[lowestIndex];
         
+        //are we at the end?
         if(current.x == end.x && current.y == end.y)
         {
+            //create the path
             var curr = current;
             while(curr.previous){
                 path.push(curr);
@@ -42,15 +43,17 @@ function THINK(player,enemies,maplayout,end)
             pathFound = true;
         }
         
+        //move current tile to visited tiles
         removeFromArray(openList, current);
         closedList.push(current);
         
         var neighbors = current.neighbors;
         
-        
+        //check neighbors for distances
         for(var j = 0; j<neighbors.length; j++)
         {
             var n = neighbors[j];
+            //if visited or is a wall, skip
             if(closedList.includes(n) || n.wall)
                 continue;
             else
@@ -58,17 +61,19 @@ function THINK(player,enemies,maplayout,end)
                 var tempG = current.g + 1;
                 var isBest = false;
                 
+                //if you havent visited it yet
                 if(!openList.includes(n))
                 {
                     isBest = true;
                     n.h = heuristic(n, end);
                     openList.push(n);
                 }
+                //if that cost is worse than what you have
                 else if(tempG < n.g)
                 {
                     isBest = true;
                 }
-                
+                //if you found a better cost
                 if(isBest)
                 {
                     n.previous = current;
@@ -80,9 +85,10 @@ function THINK(player,enemies,maplayout,end)
         }
         
     }
-    
+    //if the path is made
     else if(pathFound)
         {
+            //check if the player position is the same as the previous path tile
             if(index == 0 || path[index-1].x == player.getX() && path[index-1].y == player.getY())
                 move(player, maplayout, end, path);
         }
@@ -93,17 +99,17 @@ function THINK(player,enemies,maplayout,end)
 // bots intelligence!
 function initAI (player,enemies,maplayout,end)
 {
-    pathFound = false;
-	start = [player.getX(), player.getY()];
-	grid = new Array(maplayout.length);
-    cols = maplayout.length;
-    rows = maplayout[0].length;
-    hasMoved = true;
-	index = 0;
+    pathFound = false; //is there a path already?
+	grid = new Array(maplayout.length); //creating each row
+    cols = maplayout.length; //column count
+    rows = maplayout[0].length; //row count
+	index = 0; //index for path
     
+    //creating each column
 	for(var w = 0; w <maplayout.length; w++)
 		grid[w] = new Array(maplayout[w].length);
 	
+    //setting each tile
 	for(var x = 0; x < maplayout.length; x++)
 		for(var y = 0; y < maplayout[x].length; y++)
 		{
@@ -112,61 +118,45 @@ function initAI (player,enemies,maplayout,end)
 			else
 				grid[x][y] = new Spot(x, y, false);
 		}
+    
+    //setting neighbors
     for(var a = 0; a < maplayout.length; a++)
         for(var b = 0; b <maplayout[a].length; b++)
             grid[a][b].addNeighbors(grid);
-            
-	this.end = end;
-	movePrev = grid[player.getX() + 1][player.getY()];
-	
+    
+    //The start tile
     openList.push(grid[player.getX()][player.getY()]);
 }
 
+//Actually moving the bot
 function move(player, maplayout, end, path)
 {
     
-            moveCur = grid[player.getX()][player.getY()];
+    moveCur = grid[player.getX()][player.getY()];
             
     next=  path[index];
     
-    console.log(next);
+    //Figuring out the relations of 2 spots
     if (next.y < moveCur.y)
-        {
-            console.log(0);
-            nextMove = 0;
-        }
+        nextMove = 0;
     else if(next.y > moveCur.y)
-    {
-            console.log(1);
-            nextMove = 1;
-        }
-        
+        nextMove = 1;
     else if(next.x < moveCur.x)
-        {
-            console.log(2);
-            nextMove = 2;
-        }
+        nextMove = 2;
     else if (next.x > moveCur.x)
-        {
-            console.log(3);
-            nextMove = 3;
-        }
+        nextMove = 3;
     
-    
-
-
-    switch(nextMove){ // do your decided move
-        case 0: hasMoved = player.MoveUp(); break;
-        case 1: hasMoved = player.MoveDown(); break;
-        case 2: hasMoved = player.MoveLeft(); break;
-        case 3: hasMoved = player.MoveRight(); break;
+    switch(nextMove){ // Moving the bot
+        case 0: player.MoveUp(); break;
+        case 1: player.MoveDown(); break;
+        case 2: player.MoveLeft(); break;
+        case 3: player.MoveRight(); break;
     }
 
-    movePrev = moveCur;
-        
     index++;
 }
 
+//Manhattan distance
 function heuristic(current, end)
 {
     var d1 = Math.abs(end.x - current.x);
@@ -175,6 +165,7 @@ function heuristic(current, end)
     return d1+d2;
 }
 
+//A tile in the grid
 function Spot(x,y, isWall)
 {
 	this.x = x;
@@ -182,10 +173,11 @@ function Spot(x,y, isWall)
 	this.f = 0;
 	this.g = 0;
 	this.h = 0;
-	this.previous = null;
+	this.previous = null; //The node before for a path
 	this.wall = isWall;
     this.neighbors = [];
     
+    //All neighbors, even walls
     this.addNeighbors = function (grid) {
 		var i = this.x;
 		var j = this.y;
@@ -201,13 +193,14 @@ function Spot(x,y, isWall)
 	}
 }
 
+//Removing on open list
 function removeFromArray(arr, elem) {
 	for(var i = arr.length - 1; i>=0; i--)
 		if(arr[i] == elem)
 			arr.splice(i,1);
 }
 
-
+//We need this for some reason
 function endAI(win)
 {
 
